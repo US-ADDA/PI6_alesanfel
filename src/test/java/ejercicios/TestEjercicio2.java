50ea8d7d224b801200dc8d96490f63138eb150cd
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-05-07 10:12:49 +0200

    Correigo problema en test del ejercicio 2.

--- src/test/java/ejercicios/TestEjercicio2.java
+++ src/test/java/ejercicios/TestEjercicio2.java
@@ -1,5 +1,7 @@
 package test.java.ejercicios;
 
+import java.util.Locale;
+
 import main.java.ejercicios.ejercicio2.*;
 import test.java.tools.TestAlgorithmSum;
 
@@ -8,6 +10,8 @@ public class TestEjercicio2 {
     private static final String DATA_PATH = "data/PI6Ej2DatosEntrada";
 
     public static void main(String[] args) {
+    	
+    	Locale.setDefault(new Locale("en", "US"));
 
         String[] data = {DATA_PATH + 1 + ".txt", DATA_PATH + 2 + ".txt"};
 
@@ -19,8 +23,8 @@ public class TestEjercicio2 {
                 SolutionEjercicio2::of,
                 VertexEjercicio2.constraints());
 
-        algorithms.testAStar(data);
-        algorithms.testBT(data);
+        //algorithms.testAStar(data);
+        //algorithms.testBT(data);
         algorithms.testPDR(data);
     }
 
